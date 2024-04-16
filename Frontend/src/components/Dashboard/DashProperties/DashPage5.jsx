192569cd54c243d4f189f39d5e230c3f97b20303
VicBikey <chibykev1@gmail.com>
2024-04-16 13:53:25 +0100

    added the Pricing component features

--- Frontend/src/components/Dashboard/DashProperties/DashPage5.jsx
+++ Frontend/src/components/Dashboard/DashProperties/DashPage5.jsx
@@ -1,13 +1,151 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { FaCheckCircle } from "react-icons/fa";
+import { FaTimesCircle } from "react-icons/fa";
+import { FaNairaSign } from "react-icons/fa6";
+
 
 const DashPage5 = () => {
+  const [slideIn, setSlideIn] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSlideIn(true);
+    }, 200);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <>
-      <div>
-        <h1>Page 5</h1>
+      <div className="justify-center text-[#0F6C96] ">
+        <div>
+          <h1
+            className={`text-3xl font-extrabold text-center pb-10 transition-transform duration-500 transform ${
+              slideIn ? "translate-y-0" : "translate-y-full"
+            }`}
+          >
+            FLEXIBLE & AFFORDABLE PLANS
+          </h1>
+          <h1
+            className={`text-xl font-medium text-center pb-10 text-black transition-transform duration-500 transform ${
+              slideIn ? "translate-y-0" : "translate-y-full"
+            }`}
+          >
+            Transparent and Flexible Pricing For All Your Needed Services
+          </h1>
+        </div>
+
+        <div className="Card_cont justify-center flex gap-20 sm:flex-wrap">
+          <div
+            className={`rounded-xl py-10 w-[350px] shadow-2xl shadow-[#0F6C96] bg-white transition-transform duration-500 transform ${
+              slideIn ? "translate-y-0" : "translate-y-full"
+            }`}
+          >
+            <h2 className="text-2xl font-bold px-5">FREE PLAN</h2>
+            <div className="flex px-5 items-center">
+              <h2 className="text-2xl font-bold ">
+                <FaNairaSign />
+              </h2>
+              <h2 className="text-3xl font-bold ">0</h2>
+            </div>
+            <div className="mt-2 mb-4 border-b border-solid border-[#0F6C96] px-5 "></div>
+            <div className="px-5">
+              <p>Showcase your work and Connect with potential customers.</p>
+              <div className="mt-4">
+                <p className="font-semibold pb-5">Benefits:</p>
+                <ul className="list-disc ml-6 flex flex-col gap-3 pb-3">
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>List One Artisan Ad</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Manage your profile</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Receive customer enquiries</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Free basic analysis</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaTimesCircle className="text-red-500" />
+                    <p>Priority Listing Placement</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaTimesCircle className="text-red-500" />
+                    <p>Featured Artisan badge</p>
+                  </div>
+                </ul>
+                <button className="hover:bg-[#E2EDF2] hover:font-bold border border-black rounded-3xl px-5 py-3 w-full">
+                  <span className="flex items-center justify-center gap-2">
+                    <span>Get Started</span>
+                    <FaCheckCircle className="text-[#0F6C96] " />
+                  </span>{" "}
+                </button>
+              </div>
+            </div>
+          </div>
+
+          
+          
+          <div
+            className={`rounded-xl py-10 w-[350px] shadow-2xl shadow-[#0F6C96] bg-white transition-transform duration-500 transform ${
+              slideIn ? "translate-y-0" : "translate-y-full"
+            }`}
+          >
+            <h2 className="text-2xl font-bold px-5">PREMIUM PLAN</h2>
+            <div className="flex px-5 items-center">
+              <h2 className="text-2xl font-bold ">
+                <FaNairaSign />
+              </h2>
+              <h2 className="text-3xl font-bold ">50k</h2>
+            </div>
+            <div className="mt-2 mb-4 border-b border-solid border-[#0F6C96] px-5 "></div>
+            <div className="px-5">
+              <p>Maximize your reach and grow your business</p>
+              <div className="mt-4">
+                <p className="font-semibold pb-5">Benefits:</p>
+                <ul className="list-disc ml-6 flex flex-col gap-3 pb-3">
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>List Unlimited Artisan Ads</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Manage your profile</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Receive customer enquiries</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Advanced analytics</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Priority Listing Placement</p>
+                  </div>
+                  <div className="flex gap-2 items-center ">
+                    <FaCheckCircle className="text-[#0F6C96]" />
+                    <p>Featured Artisan badge</p>
+                  </div>
+                </ul>
+                <button className="hover:bg-[#E2EDF2] hover:font-bold border border-black rounded-3xl px-5 py-3 w-full">
+                  <span className="flex items-center justify-center gap-2">
+                    <span>Get Started</span>
+                    <FaCheckCircle className="text-[#0F6C96] " />
+                  </span>{" "}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
-  )
-}
-
-export default DashPage5
+  );
+};
+export default DashPage5;
