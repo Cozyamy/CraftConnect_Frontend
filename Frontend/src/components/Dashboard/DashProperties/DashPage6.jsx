c89fd53cedccd6b91120d3c06f8b4c8732dfc5d4
VicBikey <chibykev1@gmail.com>
2024-04-17 13:49:47 +0100

    updated Dashboard6 page

--- Frontend/src/components/Dashboard/DashProperties/DashPage6.jsx
+++ Frontend/src/components/Dashboard/DashProperties/DashPage6.jsx
@@ -61,8 +61,8 @@ const DashPage6 = () => {
   };
 
   return (
-    <div className="flex flex-row justify-center items-center w-full">
-      <div className="w-[650px] bg-white pl-8 pt-14 pr-8 rounded-lg shadow-lg shadow-[#0F6C96] ">
+    <div className="flex items-center justify-center  sm:max-w-full w-[650px] ">
+      <div className="bg-white pl-8 pt-14 pr-8 rounded-lg shadow-lg shadow-[#0F6C96] ">
         <h2 className="text-2xl font-semibold mb-10">Leave a Review</h2>
         <div className="mb-4 flex items-center">
           <button
